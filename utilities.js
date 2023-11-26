5650a04d5bcfe69c48f5a28580569997165933b6
daniperil <daniperil@hotmail.com>
2023-11-26 15:55:46 -0500

    js

--- utilities.js
+++ utilities.js
@@ -4,7 +4,7 @@ function changeAudio() {
 
     // Check the current source and switch to the other audio file
     if (source.src === 'Acid Hues (Off the Hook)- Splatoon 2 OST.mp3') {
-        source.src = 'ムーンライト伝説 - DALI'; // Change to the second audio file
+        source.src = 'ムーンライト伝説 - DALI.mp3'; // Change to the second audio file
     } else {
         source.src = 'Acid Hues (Off the Hook)- Splatoon 2 OST.mp3'; // Change back to the first audio file
     }
