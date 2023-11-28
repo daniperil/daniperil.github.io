e6e044cfff6e606eb683b0db0cfc8a3d91333050
daniperil <daniperil@hotmail.com>
2023-11-28 14:59:15 -0500

    Auto commit

--- utilities.js
+++ utilities.js
@@ -4,28 +4,28 @@ const nextBtn = document.getElementById('nextBtn');
 
 // Define your playlist or array of audio sources
 const playlist = [
-  'Acid Hues (Off the Hook)- Splatoon 2 OST.mp3',
-  'ムーンライト伝説 - DALI.mp3',
-  'audiofile3.mp3'
+    'Acid Hues (Off the Hook)- Splatoon 2 OST.mp3',
+    'Moonlight.mp3',
+    'audiofile3.mp3'
 ];
 let currentTrackIndex = 0;
 
 // Function to play the current track
 function playTrack() {
-  audio.src = playlist[currentTrackIndex];
-  audio.play();
+    audio.src = playlist[currentTrackIndex];
+    audio.play();
 }
 
 // Event listener for the "Next" button
 nextBtn.addEventListener('click', () => {
-  currentTrackIndex = (currentTrackIndex + 1) % playlist.length;
-  playTrack();
+    currentTrackIndex = (currentTrackIndex + 1) % playlist.length;
+    playTrack();
 });
 
 // Event listener for the "Previous" button
 prevBtn.addEventListener('click', () => {
-  currentTrackIndex = (currentTrackIndex - 1 + playlist.length) % playlist.length;
-  playTrack();
+    currentTrackIndex = (currentTrackIndex - 1 + playlist.length) % playlist.length;
+    playTrack();
 });
 
 // Play the initial track
